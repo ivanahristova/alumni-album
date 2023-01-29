0078a762d19ee25df1211e85783ee870aca45c47
Ivana Hristova <ivanahristova8@gmail.com>
2023-01-30 00:51:58 +0200

    Add foundation for home view, images and photo dto

---
+++ backend/model/photo.php
@@ -0,0 +1,11 @@
+<?php
+
+class Photo
+{
+    private $id;
+    private $class;
+    private $programme_id;
+    private $subclass;
+    private $student_group;
+    private $path;
+}
---
+++ frontend/views/home/home.js
@@ -0,0 +1,5 @@
+// --------------------------------------------------------
+// TODO:
+// function to load first 10 photos from db (by path)
+// after that there will be a button -> load more (onClick)
+// --------------------------------------------------------
