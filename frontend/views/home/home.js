70f5a638c3fa8ef8c43b97bf1c6210bf1f911b20
Ivana Hristova <ivanahristova8@gmail.com>
2023-02-01 19:09:25 +0200

    Add search view searchbar

--- frontend/views/home/home.js
+++ frontend/views/home/home.js
@@ -1,5 +1,48 @@
 // --------------------------------------------------------
 // TODO:
-// function to load first 10 photos from db (by path)
+// function to load first 5 photos from db (by path)
 // after that there will be a button -> load more (onClick)
 // --------------------------------------------------------
+
+let currentIndex = 0;
+const imagesPerPage = 5;
+
+let lastIndex = 0;
+let hasMorePhotos = true;
+
+const imageContainer = document.getElementById("image-container");
+const loadMoreButton = document.getElementById("load-more-button");
+
+function loadMore() {
+    const endIndex = currentIndex + imagesPerPage;
+    if (hasMorePhotos === false) {
+        let element = document.getElementById("no-more-images");
+        element.style.display = 'block';
+    }
+
+    const temp = currentIndex;
+    fetch('../../../backend/controllers/photos/get-photos.php?start=${currentIndex}&end=${endIndex}')
+        .then(response => response.json())
+        .then(images => {
+            images.forEach(function(image) {
+                const img = document.createElement("img");
+                img.src = image.src;
+                img.alt = image.alt;
+                imageContainer.appendChild(img);
+            });
+
+            currentIndex = endIndex;
+            lastIndex = currentIndex;
+        });
+
+    if (currentIndex === temp) {
+        hasMorePhotos = false;
+    }
+}
+
+loadMoreButton.addEventListener("click", function() {
+    loadMore();
+});
+
+// Show the first set of images
+loadMore();
